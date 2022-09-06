0be75e20355ba40f100aa1843c3974e9eb682fc1
Eli Williams <112526609+ewilliams440@users.noreply.github.com>
2022-09-06 21:40:14 +0000

    working with data exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,20 @@
+#Part 1
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+#The last one cuts off a repeating decimal
+
+#Part 2
+print()
+rate=float(input("Current exchange rate, Euro to Dollar:"))
+amount=float(input("Amount of Euros to exchange:"))
+total=amount*rate
+result=total-3
+print(result)
