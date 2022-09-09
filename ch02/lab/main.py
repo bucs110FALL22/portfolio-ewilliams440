dc1c35bfd8fbdf47502ca981e6f2a6f6b0c08407
Eli Williams <112526609+ewilliams440@users.noreply.github.com>
2022-09-09 18:48:34 +0000

    lab 2

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,4 +1,4 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
@@ -6,5 +6,14 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week=2
+print(classes_per_week, type(classes_per_week))
+cost_per_class=cost_per_week/classes_per_week
+print(cost_per_class, type(cost_per_class))
+print("the cost per class is  $", cost_per_class)
+
 
 #Part B
+colors=["red","blue","green","purple","yellow"]
+choice=random.choice(colors)
+print(choice)
