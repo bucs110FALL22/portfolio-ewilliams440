adee5c636ca7fb93c44bec2e35b2b73d8d522868
Eli Williams <112526609+ewilliams440@users.noreply.github.com>
2022-09-19 21:02:55 +0000

    new guess number (ignore other)

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,12 +1,13 @@
 import random
 answer=random.randint(1,10)
 
-
+number_guesses=0
 for i in range(3):
   guess=int(input("Guess a number between 1 and 10: "))
+  number_guesses=number_guesses+1
   if guess==answer:
-    print("correct!")
-    break;
+    print("correct! (", number_guesses, " guesses )")
+    break
   elif guess>answer:
     print("Too High")
   else:
