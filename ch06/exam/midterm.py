819a5d0d0ce40e0d5535803c39f1d963af749453
Eli Williams <112526609+ewilliams440@users.noreply.github.com>
2022-10-27 03:14:18 +0000

    done?

--- ch06/exam/midterm.py
+++ ch06/exam/midterm.py
@@ -6,14 +6,24 @@ def move(tur,x,y):
   tur.goto(x,y)
   tur.down()
 
-def drawcloud(tur,x,y,color="white",radius=15):
-  move(tur,x,y)
+def drawcloud(tur,x,y,color,radius):
+  tur.up()
+  tur.goto(x,y)
+  tur.down()
   tur.color(color)
-  for i in range (4):
-    circlength=random.randint(45,180)
-    tur.seth(0)
+  cloudup=True
+  while tur.position()[1]<(y+10) and cloudup:
+    circlength=random.randint(10,179)
+    tur.seth(90)
     tur.circle(radius,circlength)
-  tur.goto(x,tur.position()[1])
+  cloudup=False
+  while tur.position()[1]>(y+radius+2):
+    circlength=random.randint(180,255)
+    tur.seth(90)
+    tur.circle(radius,circlength)
+  tur.seth(90)
+  tur.circle(radius,180)
+  tur.goto(tur.position()[0],y)
   tur.goto(x,y)
 
 def main():
@@ -23,7 +33,7 @@ def main():
   suncolor="yellow"
   sunradius=15
   cloudcolor="white"
-  cloudradius=15
+  cloudradius=7
   window=turtle.Screen()
   maxxcoord=window.screensize()[0]/2
   maxycoord=window.screensize()[1]/2
@@ -39,10 +49,11 @@ def main():
   move(turtboy,-maxxcoord+50,maxycoord-50)
   turtboy.color(suncolor)
   turtboy.circle(sunradius)
-  window.exitonclick()
   #draw clouds
-  drawcloud(turtboy,100,10)
-
+  drawcloud(turtboy,0,60,cloudcolor,cloudradius)
+  drawcloud(turtboy,-150,10,cloudcolor,cloudradius)
+  drawcloud(turtboy,150,-10,cloudcolor,cloudradius)
+  window.exitonclick()
 
 main()
 #things so far-- turtle seth, circle
